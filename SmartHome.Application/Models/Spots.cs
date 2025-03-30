291b49e21b89310a08bb93f60434377d596b6d31
Melikyan Rafael <melikyanrafael13@gmail.com>
2025-03-30 21:18:29 +0300

    Удалил конструктор, который должен был удалиться в прошлом коммите

--- SmartHome.Application/Models/Spots.cs
+++ SmartHome.Application/Models/Spots.cs
@@ -6,10 +6,5 @@ namespace SmartHome.Application.Models
         public Guid ID { get; init; }
         public string Name { get; init; } = "Споты";
         public DevicesType Type { get; init; } = DevicesType.Spots;
-        public Spots(int id, string name)
-        {
-            this.ID = id;
-            this.Name = name;
-        }
     }
 }
