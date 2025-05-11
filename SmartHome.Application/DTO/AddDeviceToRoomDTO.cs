987e26412e3e61a10269c30b9550e85a8dc11135
Rafael Melikyan <melikyanrafael13@gmail.com>
2025-05-11 23:05:46 +0300

    Добавил DTO

---
+++ SmartHome.Application/DTO/AddDeviceToRoomDTO.cs
@@ -0,0 +1,13 @@
+﻿using SmartHome.Data.Enumerables;
+
+namespace SmartHome.Application.DTO
+{
+    public class AddDeviceToRoomDTO
+    {
+        public Guid ID { get; set; }
+        public Guid RoomId { get; set; }
+        public DevicesType DeviceType { get; set; }
+        public string DeviceName { get; set; }
+        public double? WorkTemperature { get; set; }
+    }
+}
