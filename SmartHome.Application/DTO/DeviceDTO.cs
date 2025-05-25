54ceb7b377d324301c1524dda9e62c9c27b544a2
Rafael Melikyan <melikyanrafael13@gmail.com>
2025-05-25 22:40:03 +0300

    Добавил DTO для передачи данных клиенту

---
+++ SmartHome.Application/DTO/DeviceDTO.cs
@@ -0,0 +1,12 @@
+﻿using SmartHome.Data.Enumerables;
+
+namespace SmartHome.Application.DTO
+{
+    public class DeviceDTO
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; } = "";
+        public DevicesType Type { get; set; }
+        public double? WorkTemperature { get; set; }
+    }
+}
---
+++ SmartHome.Application/DTO/RoomDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartHome.Application.DTO
+{
+    public class RoomDTO
+    {
+        public Guid ID { get; init; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public List<DeviceDTO> Devices { get; set; } = [];
+    }
+}
